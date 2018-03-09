48a72a7c360fb9e1e47b46fd1c6dde855d90f410
Jim McGee <jmcgee86@gmail.com>
2018-03-09 00:10:54 +0000

    completed walkthrough

--- app.js
+++ app.js
@@ -1,12 +1,59 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT;
+var path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(port, function(){
     console.log("Express is Running");
 });
 
 app.get('/', function(req, res){
-  res.send("Hello, CareerDevs!");
 });
 
+app.get('/hello', function(req,res){
+  res.send("Hello, CareerDevs!")
+})
+ 
+ app.get('/student/:name', function(req,res){
+  res.send("My name is " + req.params.name)
+})
+
+app.get('/speak/:animal', function(req,res){
+  var sounds = {
+    dog: "Woof",
+    sheep: "Baah",
+    cow: "Moooo",
+    cat: "Meow",
+    fox: "what do i say?"
+    
+  }
+      var animal = req.params.animal.toLowerCase();
+      var sound = sounds[animal];
+      
+      res.send("The " + animal + " says " + sound);
+
+})
+
+
+app.get('/speak/:animal/:duration', function(req,res){
+  var sounds = {
+    dog: "Woof",
+    sheep: "Baah",
+    cow: "Moooo",
+    cat: "Meow",
+    fox: "what do i say?"
+    
+  }
+      var animal = req.params.animal.toLowerCase();
+      var sound = sounds[animal];
+      var noise = ""
+      var duration = parseInt(req.params.duration)
+      
+      for (var i=0; i<duration; i++){
+        noise += sound + " "
+      }
+      
+      res.send("The " + animal + " says " + noise);
+
+})
